75d9d60b91c83acf9c3650a341152f6a49afaa74
Alex Lloreda <alexlloreda@gmail.com>
2016-03-27 10:21:30 +1100

    Week6: Secondary replica with replication protocol

--- kvstore/src/main/scala/kvstore/Replica.scala
+++ kvstore/src/main/scala/kvstore/Replica.scala
@@ -52,10 +52,10 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
 
   override def receive = {
     case JoinedPrimary   => context.become(leader)
-    case JoinedSecondary => context.become(replica)
+    case JoinedSecondary => context.become(replica(0))
   }
 
-  /* TODO Behavior for  the leader role. */
+  /* Behavior for  the leader role. */
   val leader: Receive = {
     case Insert(k, v, id) => {
       kv = kv.updated(k,v)
@@ -68,10 +68,29 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
     case Get(key, id) => sender ! GetResult(key, kv.get(key), id)
   }
 
-  /* TODO Behavior for the replica role. */
-  val replica: Receive = {
-    case _ =>
+  private def updateKV(k: String, vOption: Option[String]) = vOption match {
+    case None => kv = kv - k
+    case Some(v) => kv = kv.updated(k, v)
+  }
+
+  /* Behavior for the replica role. */
+  def replica(expected: Long): Receive = {
+    // KV Protocol
+    case Get(key, id) => sender ! GetResult(key, kv.get(key), id)
+
+    // Replication Protocol
+    case Replicate(key, valueOption, id) => {
+      updateKV(key, valueOption)
+      sender ! Replicated(key, id)
+    }
+    case Snapshot(key, valueOption, seq) =>
+      if (seq < expected) sender ! SnapshotAck(key, seq)
+      else if (seq == expected) {
+        updateKV(key, valueOption)
+        context.become(replica(expected + 1))
+        sender ! SnapshotAck(key, seq)
+      }
+      // Ignore any request with seq > expected
   }
 
 }
-
